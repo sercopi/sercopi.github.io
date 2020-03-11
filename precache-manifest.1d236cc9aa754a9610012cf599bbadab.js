6760bfa60d1b0aa3e8994d35a48301bd3e5233f9
Akuma <sergiiosercopi@gmail.com>
2020-03-11 23:05:18 +0100

    created menu and basic style filesystem

---
+++ precache-manifest.1d236cc9aa754a9610012cf599bbadab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168c4a0358ec4b677cefb470410c22e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2c44bd5d5f2fbd78a70",
+    "url": "/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "7c97f0809a960b296da3",
+    "url": "/static/js/2.8f2439dc.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/static/js/2.8f2439dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2c44bd5d5f2fbd78a70",
+    "url": "/static/js/main.749e7236.chunk.js"
+  },
+  {
+    "revision": "514361aa1f0c166043dd",
+    "url": "/static/js/runtime-main.eb51ea5e.js"
+  }
+]);
